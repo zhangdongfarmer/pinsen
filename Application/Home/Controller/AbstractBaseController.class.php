f71e591d55e71dc112135bf4786ad534b6462508
tangguosheng <tanggs@51good.cn>
2015-06-08 18:26:14 +0800

    task--添加接口定义类

---
+++ Application/Home/Controller/AbstractBaseController.class.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * 接口基类
+ * 
+ * @version 2015-6-6
+ * @author tangguosheng <tanggsh@163.com>
+ */
+
+namespace Home\Controller;
+use \Think\Controller;
+
+abstract class AbstractBaseController extends Controller 
+{
+    
+    /**
+     * 权限验证
+     * 
+     * @param array $itemArr 加密规则字段，招次序组合
+     * @return bool
+     */
+    protected function checkToken($itemArr)
+    {
+        
+    }
+}
+
+?>
